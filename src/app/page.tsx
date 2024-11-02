689d2a2a33cba38938c7a5b765b8f7c26d8ccfb3
Hamza Shabir <hamzii2205@gmail.com>
2024-11-03 00:57:58 +0500

    add til 19

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 // import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Link href="/blog">Blog</Link>
+      {/* <Link href={"/blog"}>Blog</Link> both are correct method to navigate links */}
+      <Link href="/products">Products</Link>
     </div>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,23 @@
 // we will see Dynamic routes in this folder
 // and as well as we will see nested dynamic routes
-
+import Link from "next/link";
 export default function ProductList() {
   return (
     <>
-      <h1>Product List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href={"/"}>Home</Link>
+      <h1>Products List</h1>
+      <h2>
+        <Link href={"products/1"}>Product 1</Link>
+      </h2>
+      <h2>
+        <Link href={"products/2"}>Product 2</Link>
+      </h2>
+      <h2>
+        <Link href={"products/3"} replace>
+          Product 3
+        </Link>{" "}
+        {/*replace is act as a back button let's see on browser*/}
+      </h2>
     </>
   );
 }
