e619993d5aeada19fea4f51286e5efa53df74335
Hamza Shabir <hamzii2205@gmail.com>
2025-02-11 03:46:04 +0500

    add post req

--- src/app/getreq/api/route.tsx
+++ src/app/getreq/api/route.tsx
@@ -1,11 +1,24 @@
-// import { NextResponse } from "next/server";
-
-// export async function GET(req: Request) {
-//   return NextResponse.json({ message: "hello i am here" });
-// }
-var myData: any[] = [
-  { name: "Hamza", age: "20", qualification: "Intermediate" },
-];
+import { NextResponse } from "next/server";
+let myData: any[] = [{ name: "Hamza", age: 20, qualification: "Intermediate" }];
 export async function GET() {
-  return Response.json(myData);
+  return NextResponse.json(myData);
+}
+
+export async function POST(req: Request) {
+  const data = await req.json();
+
+  const newData = {
+    age: myData.length + 1, // Assuming you want to increment the ID based on the array length
+    name: data.name,
+    qualification: data.qualification,
+  };
+
+  myData.push(newData);
+
+  return NextResponse.json(newData, {
+    status: 201,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
